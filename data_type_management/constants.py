c4075d866547e738b1fb004bcd74494805eccf42
arunp250 <arun.patil@avetoconsulting.com>
2021-02-06 22:45:30 +0530

    ugfygd

--- data_type_management/constants.py
+++ data_type_management/constants.py
@@ -1,6 +1,7 @@
-invalid_strings = ['', None]
-create_success_message = 'operation created successfully'
-delete_success_message = ' object deleted successfully'
-delete_failure_message = ' object does not exist'
+invalid_strings = ['', '', '', '', None]
+# create_success_message = 'operation created successfully'
+# delete_success_message = ' object deleted successfully'
+delete_failure_message = ' object does not exist '
 name_not_valid = 'Please enter valid name'
 something_went_wrong = 'Something went wrong.'
+well_well = ''
