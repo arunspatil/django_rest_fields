d3f6b0240b8d1bfd8520e738bfa4fff4cc6135d2
arunp250 <arun.patil@avetoconsulting.com>
2021-02-06 22:33:45 +0530

    mm

--- data_type_management/constants.py
+++ data_type_management/constants.py
@@ -2,5 +2,5 @@ invalid_strings = ['', None]
 create_success_message = 'operation created successfully'
 delete_success_message = ' object deleted successfully'
 delete_failure_message = ' object does not exist'
-name_not_valid = 'Please enter valid name'
-something_went_wrong = 'Something went wrong.'
+name_not_valid = 'Please enter valid name.'
+something_went_wrong = 'Something went wrong'
